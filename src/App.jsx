b87c1d949c9b3a35a4e8cad875aff362ee29c1a1
sanyam <patilsanyam869@gmail.com>
2024-01-18 17:35:09 +0530

    initial update header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Layout from './components/layout/Layout'
+import Header from './components/header/Header'
 
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Layout>
+
+    <Header/>
+          {/* <Layout>
 
         <h1 className='bg-red-200'>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis molestiae omnis aut sunt nam, minima pariatur consectetur doloremque odio officia.</p>
